9c326085869fba0f0ffecb2085a4e245aa1faa91
pradip0016 <patilpradip980@gmail.com>
2020-09-28 09:13:36 +0530

    UC6 PlayerChoice

--- Game.java
+++ Game.java
@@ -1,11 +1,14 @@
 package tictoc.com;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
 	Scanner s = new Scanner(System.in);
 	char[] board = new char[10];
 	char PlayerMark;
+	boolean endgame = true;
+	String playername,name;
 	
 	/* UC1 Initialize Array */
 	public void createBoard() {
@@ -40,8 +43,11 @@ public class Game {
 		System.out.println("\n");
 	}
 int row;
-	public void movePosition() {
+	public void movePosition(String pl) {
 		while (true) {
+			System.out.println("Player "+pl+" Enter row number");
+			row = s.nextInt();
+			
 			if (row < 0 || row > 9) {
 				System.out.println("Your row and col out of Board");
 			} else if (board[row] != ' ') {
@@ -50,8 +56,35 @@ int row;
 				char value = determineInput();
 				board[row] = value;
 				showBoard();
-				
 			}
+			if(pl.equalsIgnoreCase(name))
+	    	{
+	    		pl="computer";
+	    	}
+	    	else
+	    	{
+	    		pl=name;
+	    	}
 	  }
 }
+	/* UC5 player choice */
+	public String chance() {
+		int result;
+		Random randomNum = new Random();
+		result = randomNum.nextInt(2);
+		System.out.println("Enter First Player name");
+		name=s.next();
+		System.out.println(name+" enter 0 for Head and 1 for Tail");
+		int ch = s.nextInt();
+		if (result == ch) {
+			System.out.println(name +"Play First");
+			playername = name;
+			return playername;
+		} else {
+			System.out.println("Computer Play First");
+			playername = "computer";
+		return playername;
+		}
+	}
+	
 }
--- TicTocGame.java
+++ TicTocGame.java
@@ -4,11 +4,12 @@ package tictoc.com;
 public class TicTocGame {
 
 	public static void main(String[] args) {
-	
+		String pl;
 		Game g = new Game();
+		pl=g.chance();
 		g.createBoard();
 		g.showBoard();
-		g.movePosition();
+		g.movePosition(pl);
 	}
 
 }
