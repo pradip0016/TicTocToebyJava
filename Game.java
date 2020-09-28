e21b6985ca9c7babfb668994c2fbd5f6b94aba64
pradip0016 <patilpradip980@gmail.com>
2020-09-28 08:43:21 +0530

    UC1 InitializeBoard

--- Game.java
+++ Game.java
@@ -1,5 +1,14 @@
 package tictoc.com;
 
 public class Game {
+	char[] board = new char[10];
 
+	/* UC1 Initialize Array */
+	public void createBoard() {
+
+		for (int i = 1; i < board.length; i++) {
+			board[i] = ' ';
+
+		}
+	}
 }
--- TicTocGame.java
+++ TicTocGame.java
@@ -1,10 +1,12 @@
 package tictoc.com;
 
+
 public class TicTocGame {
 
 	public static void main(String[] args) {
 	
-
+		Game g = new Game();
+		g.createBoard();
 	}
 
 }
